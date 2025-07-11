5fa6f16b726ff0323545ef6e2e71aaf1bc519fce
llol111 <llol111@xn--nurfrspam-t9a.de>
2025-07-11 23:51:56 +0200

    movement is a thing
     lmao

--- player.gd
+++ player.gd
@@ -8,4 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	position += input_direction * 50 * delta
