3063d3ccb547fd4a22a94a15581757e07157c9a5
TomboyAprechiator <sexismylifeshinji@tutanota.com>
2025-07-14 23:04:12 +0200

    bullets can be pushed around on the floor and shit
    mhm

--- player.gd
+++ player.gd
@@ -11,3 +11,12 @@ func _process(delta: float) -> void:
 	var input_direction = Input.get_vector("left","right","up","down")
 	velocity = input_direction * 150
 	move_and_slide()
+	
+
+func _on_push_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("bullets"):
+		var diff = body.global_position - global_position
+		print("Diff vector:", diff)
+		var direction = diff.normalized()
+		print("Direction:", direction)
+		body.apply_impulse(direction*1000,Vector2.ZERO)
