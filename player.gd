4bc75329e4d699b70474b194d361af2235a910c3
TomboyAprechiator <sexismylifeshinji@tutanota.com>
2025-07-19 23:04:16 +0200

    Tileset is a thing
    some othert things also maybe i dont member

--- player.gd
+++ player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@export var movespeed = 150
+@export var gravity = 400
+@export var jumpforce = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,11 +11,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var input_direction = Input.get_vector("left","right","up","down")
-	velocity = input_direction * 150
-	move_and_slide()
+	pass
 	
-
+func _physics_process(delta):
+	#gravity
+	velocity.y += gravity * delta
+	
+	#lr movement
+	var lr_direction = 0
+	if Input.is_action_pressed("left"):
+		lr_direction = -1
+	if Input.is_action_pressed("right"):
+		lr_direction = 1
+	
+	velocity.x = lr_direction * movespeed
+	
+	#jump
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = -jumpforce
+	
+	#process
+	move_and_slide()
 func _on_push_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("bullets"):
 		var diff = body.global_position - global_position
